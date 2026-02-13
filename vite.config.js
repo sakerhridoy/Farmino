c8210c5010fb586b66530d624ce4aaf79f7c4d9b
hridoy <sakerahmedhridoy@gmail.com>
2026-02-13 18:26:10 +0600

    responsive

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        shop: resolve(__dirname, 'shop.html'),
+      },
+    },
+  },
 });

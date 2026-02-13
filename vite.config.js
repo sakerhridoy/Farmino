17e453c6ac8d1e90fc8c8d993cdde2de16bc8f9b
hridoy <sakerahmedhridoy@gmail.com>
2026-02-13 18:32:50 +0600

    fix error

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import { resolve } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 export default defineConfig({
   plugins: [tailwindcss()],
